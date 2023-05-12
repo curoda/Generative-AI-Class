20715571e2609c245eae8fa050bf0fa113c94b30
Curt Odar <curoda@users.noreply.github.com>
2023-05-12 15:49:34 -0400

    Update Streamlit-Get-Reviews.py
    reverting to earlier version

--- Streamlit-Get-Reviews.py
+++ Streamlit-Get-Reviews.py
@@ -7,7 +7,7 @@ import openai
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 def generate_buy_score(review):
-    prompt = f"Based on the following review details, generate a Buy Score between 0 and 100, where 0 means 'don't buy' and 100 means 'definitely buy':\n\nTitle: {review.get('reviewTitle', 'N/A')}\nDescription: {review.get('reviewDescription', 'N/A')}\nStars: {review.get('ratingScore', 'N/A')}\nReaction: {review.get('reviewReaction', 'N/A')}\n\nBuyScore:"
+    prompt = f"Based on the following review details, generate a Buy Score between 0 and 100, where 0 means 'don't buy' and 100 means 'definitely buy':\n\nTitle: {review['reviewTitle']}\nDescription: {review['reviewDescription']}\nStars: {review['ratingScore']}\nReaction: {review['reviewReaction']}\n\nBuy Score:"
 
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -15,7 +15,7 @@ def generate_buy_score(review):
         max_tokens=10,
         n=1,
         stop=None,
-        temperature=0.3,
+        temperature=0.5,
     )
 
     try:
@@ -49,16 +49,15 @@ def get_amazon_reviews(url, max_reviews):
         'reaction': item.get('reviewReaction', 'N/A'),
         'description': item.get('reviewDescription', 'N/A')
         }
-        if review['reaction'] != 'N/A':  # Only add reviews with a reaction
-            reviews.append(review)
+        reviews.append(item)
 
     return reviews
 
-st.title("Amazon Buy Score Generator")
+st.title("Amazon Reviews Scraper")
 st.write("Enter the Amazon product URL and set the maximum number of reviews to fetch:")
 
 url = st.text_input("Amazon product URL:", "")
-max_reviews = st.number_input("Max reviews:", min_value=1, value=10)
+max_reviews = st.number_input("Max reviews:", min_value=1, value=100)
 
 if st.button("Fetch reviews"):
     if url:
@@ -67,25 +66,19 @@ if st.button("Fetch reviews"):
             st.write("Reviews fetched successfully!")
             # Initialize the total_score variable
             total_score = 0
-            processed_reviews = 0
             for review in reviews:
                 buy_score = generate_buy_score(review)
                 total_score += buy_score
                 
-                st.write(f"Stars: {review.get('ratingScore', 'N/A')}")
-                st.write(f"Title: {review.get('reviewTitle', 'N/A')}")
-                st.write(f"Reaction: {review.get('reviewReaction', 'N/A')}")
-                st.write(f"Description: {review.get('reviewDescription', 'N/A')}")
+                st.write(f"Stars: {review['ratingScore']}")
+                st.write(f"Title: {review['reviewTitle']}")
+                st.write(f"Helpful: {review['reviewReaction']}")
+                st.write(f"Description: {review['reviewDescription']}")
                 st.write(f"Buy Score: {buy_score}")
                 st.write("---")
-                processed_reviews += 1
-            if processed_reviews > 0:
-                average_buy_score = total_score / processed_reviews
-                st.write(f"Average Buy Score: {average_buy_score:.2f}")
-            else:
-                st.write("No reviews were processed due to lack of reactions.")
+            average_buy_score = total_score / len(reviews)
+            st.write(f"Average Buy Score: {average_buy_score:.2f}")
         else:
             st.write("No reviews found.")
     else:
         st.write("Please enter a valid Amazon product URL.")
-
