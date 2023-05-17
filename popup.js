4529a4e929151a84af73003badf4ac7679feaf3e
Curt Odar <curoda@users.noreply.github.com>
2023-05-17 11:26:43 -0400

    Update popup.js
    updating to streamline code

--- popup.js
+++ popup.js
@@ -1,83 +1,61 @@
 // popup.js
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {message: "get_product_data"}, function(response) {
+const OPEN_AI_API = 'https://api.openai.com/v1/completions';
+const MAX_TOKENS = 100;
+const MODEL = 'text-davinci-003';
+const API_KEY = 'YOUR_API_KEY'; // Replace with your actual API key
+
+chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {message: "get_product_data"}, async function(response) {
     document.getElementById('product-title').textContent = response.productTitle;
     document.getElementById('price').textContent = response.price;
     document.getElementById('rating').textContent = response.rating;
     document.getElementById('reviews').textContent = response.reviews;
 
-    getSummary(response.summary, response.features)
-    .then(summary => {
- 	    document.getElementById('summary').textContent = summary;
-    });
+    try {
+      const summary = await getSummary(response.summary, response.features);
+      document.getElementById('summary').textContent = summary;
 
-    getBuyScore(response)
-    .then(data => {
- 	    document.getElementById('buy').textContent = data;
-    });
+      const buyScore = await getBuyScore(response);
+      document.getElementById('buy').textContent = buyScore;
+    } catch(error) {
+      console.error("Error in fetching data from OpenAI API", error);
+    }
   });
 });
 
-
-function getSummary(summary, features){
-  const joinedText = `${summary}\n${features.join(', ')}`;
-  const prompt = `summarize the following product description in 100 words or less: ${joinedText}`;
-  const maxTokens = 100;
-
-  return fetch('https://api.openai.com/v1/completions', {
+async function callOpenAiApi(prompt) {
+  const response = await fetch(OPEN_AI_API, {
     method: 'POST',
     headers: {
-      'Authorization': `Bearer API_KEY`,
+      'Authorization': `Bearer ${API_KEY}`,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      model: 'text-davinci-003',
+      model: MODEL,
       prompt: prompt,
-      max_tokens: maxTokens
+      max_tokens: MAX_TOKENS
     })
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (data.error) {
-      return "data error:" + data.error.message;
-    } else if (data.choices && data.choices.length > 0 && data.choices[0].text) {
-      return data.choices[0].text.trim();
-    } else {
-      return "No Completion";
-    }
-  })
-  .catch(error => {
-    return "error:" + error;
   });
+
+  const data = await response.json();
+  if (data.error) {
+    throw new Error(data.error.message);
+  } else if (data.choices && data.choices.length > 0 && data.choices[0].text) {
+    return data.choices[0].text.trim();
+  } else {
+    throw new Error("No Completion from OpenAI API");
+  }
+}
+
+function getSummary(summary, features) {
+  const joinedText = `${summary}\n${features.join(', ')}`;
+  const prompt = `summarize the following product description in 100 words or less: ${joinedText}`;
+
+  return callOpenAiApi(prompt);
 }
 
 function getBuyScore(review){
-	const prompt = `Based on the following review details, generate a Buy Score between 0 and 100, where 0 means 'don't buy' and 100 means 'definitely buy':\n\nTitle: ${review.productTitle}\nDescription: ${review.summary}\nStars: ${review.rating}\nReaction: ${review.reviews}\n\nBuy Score:`;
-	const maxTokens = 100;
+  const prompt = `Based on the following review details, generate a Buy Score between 0 and 100, where 0 means 'don't buy' and 100 means 'definitely buy':\n\nTitle: ${review.productTitle}\nDescription: ${review.summary}\nStars: ${review.rating}\nReaction: ${review.reviews}\n\nBuy Score:`;
 
-	return fetch('https://api.openai.com/v1/completions', {
-	  method: 'POST',
-	  headers: {
-		'Authorization': `Bearer API_KEY`,
-		'Content-Type': 'application/json'
-	  },
-	  body: JSON.stringify({
-		model: 'text-davinci-003',
-		prompt: prompt,
-		max_tokens: maxTokens
-	  })
-	})
-	.then(response => response.json())
-	.then(data => {
-		if (data.error) {
-			return "data error:" + data.error.message;
-		} else if (data.choices && data.choices.length > 0 && data.choices[0].text) {
-			return data.choices[0].text.trim();
-		} else {
-			return "No Completion";
-		}
-	})
-	.catch(error => {
-		return "error:" + error;
-	});
+  return callOpenAiApi(prompt);
 }
