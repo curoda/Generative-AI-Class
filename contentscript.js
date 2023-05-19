59dc893faa737163cb18f647511016f3ea10b10a
Curt Odar <curoda@users.noreply.github.com>
2023-05-19 12:07:47 -0400

    Update contentscript.js
    updated to get new reviews array.

--- contentscript.js
+++ contentscript.js
@@ -12,22 +12,29 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     let price = `${priceSymbol}${priceWhole}.${priceFraction}`;
 
     let rating = document.querySelector('#averageCustomerReviews span')?.innerText;
-    let reviews = document.querySelector('#acrCustomerReviewText')?.innerText;
 
     // Get current URL and extract ASIN
     let url = window.location.href;
     let asinMatch = url.match(/\/dp\/([A-Z0-9]{10})/);
     let asin = asinMatch ? asinMatch[1] : null;
 
-    sendResponse({
-      productTitle: productTitle,
-      summary: summary,
-      features: features,
-      price: price,
-      rating: rating,
-      reviews: reviews,
-      asin: asin
+    // Request reviews from background script
+    chrome.runtime.sendMessage({message: "get_reviews", asin: asin}, response => {
+      let reviews = response.reviews;
+
+      sendResponse({
+        productTitle: productTitle,
+        summary: summary,
+        features: features,
+        price: price,
+        rating: rating,
+        reviews: reviews,
+        asin: asin
+      });
     });
+
+    return true;  // Indicate that we will respond asynchronously
   }
 });
+
 
