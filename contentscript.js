1ea8313b65d2b525be75f35269a7e8bce68d7bbd
Curt Odar <curoda@users.noreply.github.com>
2023-05-19 11:34:37 -0400

    Update contentscript.js
    updated to get the price back properly formatted, and to get the asin to help in getting reviews.

--- contentscript.js
+++ contentscript.js
@@ -4,17 +4,30 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     let productTitle = document.querySelector('#productTitle')?.innerText;
     let summary = document.querySelector('#featurebullets_feature_div')?.innerText;
     let features = Array.from(document.querySelectorAll('#feature-bullets li')).map(li => li.innerText);
-    let price = document.querySelector('.a-price-whole')?.innerText;
+
+    // Get the price symbol, whole, and fraction
+    let priceSymbol = document.querySelector('.a-price-symbol')?.innerText || "";
+    let priceWhole = document.querySelector('.a-price-whole')?.innerText || "";
+    let priceFraction = document.querySelector('.a-price-fraction')?.innerText || "";
+    let price = `${priceSymbol}${priceWhole}.${priceFraction}`;
+
     let rating = document.querySelector('#averageCustomerReviews span')?.innerText;
     let reviews = document.querySelector('#acrCustomerReviewText')?.innerText;
-    
+
+    // Get current URL and extract ASIN
+    let url = window.location.href;
+    let asinMatch = url.match(/\/dp\/([A-Z0-9]{10})/);
+    let asin = asinMatch ? asinMatch[1] : null;
+
     sendResponse({
       productTitle: productTitle,
       summary: summary,
       features: features,
       price: price,
       rating: rating,
-      reviews: reviews
+      reviews: reviews,
+      asin: asin
     });
   }
 });
+
